0e496a1ddf3703c2170ed1e9d5d9fd680e4e611b
Y0sh1dk <47013245+Y0sh1dk@users.noreply.github.com>
2020-09-15 00:01:43 +1000

    - Just sort() to go

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
 #include "LinkedList.h"
 
 
@@ -23,12 +27,14 @@ void LinkedList::addToTail(value_type& item) {
         Node* temp = new Node(item);
         head = temp;
         tail = temp;
+        start();
         temp = NULL;
     } else { // atleast one node
        Node* temp = new Node(item);
-       temp->setPrev(tail);
        tail->setNext(temp);
+       temp->setPrev(tail);
        tail = temp;
+       start();
        temp = NULL; 
     }
 }
@@ -56,14 +62,42 @@ void LinkedList::removeTail() {
 
 
 void LinkedList::removeCurrent() {
+    if (current != tail) {
+        current->getPrev()->setNext(current->getNext());
+        current->getNext()->setPrev(current->getPrev());
+        delete current;
+        start();
+    } else {
+        current->getPrev()->setNext(NULL);
+        delete current;
+        start();
+    }
 
 }
 
 void LinkedList::add(LinkedList::value_type s) {
-
+    int pos = -1;
+    for (unsigned long int i = 0; i <= s.length(); i++) {
+        if (isspace(s[i])) {
+            std::string word = s.substr(pos+1, i-pos-1);
+            // std::cout << word << std::endl;
+            pos = i;
+            addToTail(word);
+        }
+    }
+    std::string lastWord =  s.substr(pos+1, s.length());
+    addToTail(lastWord);
 }
 
 void LinkedList::remove(value_type s) {
+    start();
+    while (current != NULL) { // runs through till hits end
+        if (getCurrent() == s) {
+            removeCurrent();
+            start();
+        }
+        forward();
+    }
 
 }
 
@@ -71,21 +105,21 @@ void LinkedList::sort() {
 
 }
 
-// void LinkedList::start() {
-//     current = head;
-// }
+void LinkedList::start() {
+    current = head;
+}
 
-// void LinkedList::end() {
-//     current = tail;
-// }
+void LinkedList::end() {
+    current = tail;
+}
 
-// void LinkedList::forward() {
-//     current = current->getNext();
-// }
+void LinkedList::forward() {
+    current = current->getNext();
+}
 
-// void LinkedList::back() {
-//     current = current->getPrev();
-// }
+void LinkedList::back() {
+    current = current->getPrev();
+}
 
 LinkedList::value_type LinkedList::getCurrent() {
     return current->getData();
@@ -104,20 +138,41 @@ unsigned int LinkedList::size() {
     for (current = head; current != NULL; current = current->getNext()) {
         a++;
     }
+    start();
     return a;
 }
 
 unsigned int LinkedList::count(LinkedList::value_type s) {
-    // Count occurances of string
-    return 0;
+    start();
+    int a = 0;
+    while (current != NULL) {
+        if (getCurrent() == s) {
+            a++;
+        }
+        forward();
+    }
+    return a;
 }
 
 std::ostream& operator << (std::ostream& out, LinkedList& l) {
-
-    // Do stuff
+    l.start(); // set current pointer to head
+    std::string sentence;
+    int size = l.size();
+    for (int i = 0; i < size; i++) {
+        sentence += l.getCurrent();
+        sentence += " ";
+        l.forward();
+    }
+    out << sentence;
     return out;
 }
 
-void LinkedList::operator+= (LinkedList&) {
-
+void LinkedList::operator+= (LinkedList& l2) {
+    l2.start();
+    int l2size = l2.size();
+    for (int i = 0; i < l2size; i++) {
+        std::string temp = l2.getCurrent();
+        addToTail(temp);
+        l2.forward();
+    }
 }
